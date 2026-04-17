2c2c3e66835b6723aed29f798d23225d1afd1444
HarpistHope <harpisthope@gmail.com>
2026-04-17 10:34:50 -0500

    Final D3 Lab (final update to page title)

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@
     // define welcome popup; add close button
     welcome.html(`
         <div class="welcome-content">
-        <p><strong>Welcome to the Natural Amenities Scale Explorer!</strong></p>
+        <p><strong>Welcome to the U.S. Natural Amenities Scale Explorer!</strong></p>
         <p>Add some color to the map and place bubbles on the chart by selecting attributes from the dropdowns above.</p>
         <p>Click on a state to zoom and open additional ranking details.</p>
         <p>Hover over a state to see a summary of the selected color/bubble size attribute.</p>
@@ -551,7 +551,7 @@
             .select(".navbar")
             .append("h1")
             .attr("class", "pageTitle")
-            .text("Natural Amenities Explorer")
+            .text("U.S. Natural Amenities Scale")
     }
 
     //dropdown change event handler
